5ce98b8a3ce98f15fcd6cab7c5922ea74bfb868e
Mohomed Thahsan <thahsan@calcey.com>
2018-08-10 18:42:58 +0530

    Updated folder structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-/** @format */
-
+// @flow
 import {AppRegistry} from 'react-native';
-import App from './App';
+
+import AppComponent from './src/App.component';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppComponent);
---
+++ src/App.component.js
@@ -0,0 +1,38 @@
+// @flow
+import React from 'react';
+import {View} from 'react-native';
+import PropTypes from 'prop-types';
+import type {Element as ReactElement} from 'react';
+
+import styles from './App.styles';
+
+type AppProps = {};
+type AppState = {};
+
+class AppComponent extends React.PureComponent<AppProps, AppState> {
+  static defaultProps: any
+
+  constructor(props: AppProps) {
+    super(props);
+  }
+
+  renderContent = (): ReactElement<any> => {
+    return (
+      <View style={styles.container}>
+        {/* Add your elements here. Make sure you break the renders into clean functions */}
+      </View>
+    );
+  }
+
+  render() {
+    const content = this.renderContent();
+
+    return content;
+  }
+}
+
+AppComponent.propTypes = {};
+
+AppComponent.defaultProps = {};
+
+export default AppComponent;
