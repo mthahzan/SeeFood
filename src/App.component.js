cdb946afa26ba0f166067d5c404d450d2806bc0b
Deshan Alahakoon <deshan@calcey.com>
2018-08-12 12:25:09 +0530

    camera basic behavior

--- src/App.component.js
+++ src/App.component.js
@@ -1,9 +1,10 @@
 // @flow
 import React from 'react';
-import {View} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import PropTypes from 'prop-types';
 import type {Element as ReactElement} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
 import styles from './App.styles';
 
@@ -24,9 +25,34 @@ class AppComponent extends React.PureComponent<AppProps, AppState> {
   renderContent = (): ReactElement<any> => {
     return (
       <View style={styles.container}>
-        {/* Add your elements here. Make sure you break the renders into clean functions */}
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={'Permission to use camera'}
+          permissionDialogMessage={'We need your permission to use your camera phone'}
+        >
+          {({ camera, status }) => {
+            if (status !== 'READY') return <View />;
+            return (
+              <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={() => this.takePicture(camera)} style={styles.capture}>
+                  <Text style={{ fontSize: 14 }}> SNAP </Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        </RNCamera>
       </View>
     );
+  }
+
+
+  takePicture = async function(camera) {
+    const options = { quality: 0.5, base64: true };
+    const data = await camera.takePictureAsync(options);
+    //  eslint-disable-next-line
+    console.log(data.uri);
   }
 
   render() {
